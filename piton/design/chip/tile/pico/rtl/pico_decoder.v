95b22d3fb1fbb7a2665066e0ce255fcb30fa36d3
Jonathan Balkind <jbalkind@ucsb.edu>
2023-07-19 03:35:42 -0700

    Trying to fix some iop.h includes

--- piton/design/chip/tile/pico/rtl/pico_decoder.v
+++ piton/design/chip/tile/pico/rtl/pico_decoder.v
@@ -24,7 +24,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-`include "iop.h" 
+`include "l15.tmp.h" 
 module pico_decoder(
     input wire         clk,
     input wire         rst_n,
